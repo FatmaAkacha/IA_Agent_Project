13f711b6616e937b0be7c2bc3bef6f50b8beb8a8
FatmaAkacha <akachafatma1@gmail.com>
2026-07-28 10:42:26 +0100

    add response node

--- agent.py
+++ agent.py
@@ -3,22 +3,33 @@ from langgraph.graph import (StateGraph,END)
 
 class AgentState(TypedDict):
     question: str
+    reponse: str
 
 def analyse_node(state):
     print("Analyse de la question...")
     return state
-etat = {
-"question": "Quels sont les congés annuels ?"
-}
+
+def reponse_node(state):
+    question = state["question"]
+    state["reponse"] = ( f"Votre question est : {question}")
+    return state
+
+etat = {"question": "Quels sont les congés annuels ?"}
 analyse_node(etat)
 
 workflow = StateGraph(AgentState)
 workflow.add_node("analyse",analyse_node)
+workflow.add_node("reponse",reponse_node)
+
 
 workflow.set_entry_point("analyse")
-workflow.add_edge("analyse",END)
+workflow.add_edge("analyse","reponse")
+workflow.add_edge("reponse",END)
+
 
 agent = workflow.compile()
-resultat = agent.invoke(
-{"question":"Quels sont les congés annuels ?"})
+resultat = agent.invoke({"question":"Quels sont les congés annuels ?"})
 print(resultat)
+print(resultat["reponse"])
+
+
