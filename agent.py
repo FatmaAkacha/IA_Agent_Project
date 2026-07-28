85324ca656aa34591c166a094c91dca91e04df0c
FatmaAkacha <akachafatma1@gmail.com>
2026-07-28 11:15:50 +0100

    add third rout

--- agent.py
+++ agent.py
@@ -16,7 +16,9 @@ def reponse_node(state):
     return state
 def decision_node(state):
     question = state["question"]
-    if "+" in question:
+    if "bonjour" in question.lower():
+        state["type_question"] = ("salutation")
+    elif "+" in question:
         state["type_question"] = ("calcul")
     else:
         state["type_question"] = ("documentation")
@@ -28,6 +30,10 @@ def calculatrice_node(state):
 
 def documentation_node(state):
     state["reponse"] = ("Réponse documentaire")
+    return state
+
+def greeting_node(state):
+    state["reponse"] = ("Bonjour ! Comment puis-je vous aider ?")
     return state
 def route_question(state):
     return state["type_question"]
@@ -45,21 +51,23 @@ workflow.add_edge("analyse","decision")
 workflow.add_edge("documentation",END)
 workflow.add_edge("calculatrice",END)
 workflow.add_edge("reponse",END)
+workflow.add_edge("salutation",END)
 
 workflow.add_node("decision",decision_node)
 workflow.add_node("calculatrice",calculatrice_node)
 workflow.add_node("documentation",documentation_node)
+workflow.add_node("salutation",greeting_node)
 workflow.add_conditional_edges("decision",route_question,{
-                                "calcul":
-                                "calculatrice",
-                                "documentation":
-                                "documentation"}
+                                "salutation":"salutation",
+                                "calcul":"calculatrice",
+                                "documentation":"documentation"}
                                 )
 
 agent = workflow.compile()
 resultat = agent.invoke({"question":"Combien font 5+5 ?"})
 resultat1 = agent.invoke({"question":"Quels sont les congés ?"})
 resultat2 = agent.invoke({"question":"Combien font cinq plus trois ?"})
+resultat3 = agent.invoke({"question":"Bonjour, comment vas-tu ?"})
 
 print(resultat)
 print(resultat["reponse"])
@@ -69,5 +77,8 @@ print(resultat1["reponse"])
 
 print(resultat2)
 print(resultat2["reponse"])
+
+print(resultat3)
+print(resultat3["reponse"])
 
 
