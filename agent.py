88b9c35c7f1c853ef72ced29a3ef0b43331f3a01
FatmaAkacha <akachafatma1@gmail.com>
2026-07-29 18:06:39 +0100

    Optimize AI Assistant

--- agent.py
+++ agent.py
@@ -3,12 +3,15 @@ from langgraph.graph import StateGraph, END
 from pypdf import PdfReader
 from docx import Document
 import requests
+import time
 
 
 class AgentState(TypedDict):
     question: str
     reponse: str
     type_question: str
+    historique: str
+
 
 def calculatrice(expression):
     try:
@@ -16,108 +19,144 @@ def calculatrice(expression):
     except Exception:
         return "Expression invalide"
 
+
 def txt_reader(chemin_fichier):
     try:
-        with open(chemin_fichier, "r", encoding="utf-8") as fichier:
+        with open(chemin_fichier,"r",encoding="utf-8") as fichier:
             return fichier.read()
-    except FileNotFoundError:
+    except:
         return "Fichier introuvable."
 
+
 def pdf_reader(chemin_fichier):
-    lecteur = PdfReader(
-    chemin_fichier)
-    contenu = ""
-    for page in lecteur.pages:
-        contenu += (page.extract_text())
-    return contenu
+    try:
+        lecteur = PdfReader(chemin_fichier)
+        contenu = ""
+        for page in lecteur.pages:
+            contenu += (page.extract_text())
+        return contenu
+    except:
+        return "Fichier introuvable."
+
 
 def docx_reader(chemin_fichier):
-    doc = Document(
-    chemin_fichier)
-    contenu = ""
-    for paragraphe in doc.paragraphs:
-        contenu += (paragraphe.text + "\n")
-    return contenu
+    try:
+        doc = Document(chemin_fichier)
+        contenu = ""
+        for paragraphe in (doc.paragraphs):
+            contenu += (paragraphe.text + "\n")
+            return contenu
+    except:
+        return "Fichier introuvable."
+
 
 def analyse_node(state):
-    print("Analyse de la question...")
+    question = state["question"]
+    print("[LOG] Question reçue :",question)
     return state
+
 
 def decision_node(state):
     question = state["question"].lower()
+
     if "bonjour" in question:
-        state["type_question"] = ("salutation")
+        state["type_question"] = "salutation"
+
     elif ("+" in question or "-" in question or "*" in question or "/" in question):
-        state["type_question"] = ("calcul")
+        state["type_question"] = "calcul"
+
     elif ".pdf" in question:
-        state["type_question"] = ("pdf")
+        state["type_question"] = "pdf"
+
     elif ".docx" in question:
-        state["type_question"] = ("docx")
+        state["type_question"] = "docx"
+
     elif ".txt" in question:
-        state["type_question"] = ("txt")
+        state["type_question"] = "txt"
+
     else:
-        state["type_question"] = ("documentation")
+        state["type_question"] = "documentation"
+    print("[LOG] Outil sélectionné :", state["type_question"])
+
     return state
 
+
 def calculatrice_node(state):
-    expression = state["question"]
     expression = (
-        expression.lower()
+        state["question"]
+        .lower()
         .replace("calcule", "")
         .replace("combien font", "")
         .strip()
     )
 
-    resultat = calculatrice(expression)
-
-    state["reponse"] = str(resultat)
+    state["reponse"] = str(calculatrice(expression))
     return state
 
 
 def llm_local(prompt):
-    url = ("http://localhost:11434/api/generate")
+    url = "http://localhost:11434/api/generate"
+
     data = {
-            "model": "phi3",
-            "prompt": prompt,
-            "stream": False
-            }
-    response = requests.post(url,json=data)
+        "model": "phi3",
+        "prompt": prompt,
+        "stream": False
+    }
+
+    response = requests.post(url, json=data)
     return response.json()["response"]
+
 
 def txt_reader_node(state):
     contenu = txt_reader("documents/rh.txt")
     question = state["question"]
-    prompt = f"""Contexte :{contenu}
-    Question :{question}
-    Réponse :
-    """ 
+    prompt = f"""
+        Historique :{historique}
+        Contexte :{contenu}
+        Question :{question}
+        Réponse :
+        """
     state["reponse"] = llm_local(prompt)
     return state
 
+
 def pdf_reader_node(state):
-    contenu = pdf_reader("documents/formation.pdf" )
+    contenu = pdf_reader("documents/formation.pdf")
     question = state["question"]
-    prompt = f""" Contexte :{contenu}
-    Question :{question}
+    prompt = f"""
+        Historique :{historique}
+        Contexte :{contenu}
+        Question :{question}
     Réponse :
     """
     state["reponse"] = llm_local(prompt)
     return state
+
 def docx_reader_node(state):
     contenu = docx_reader("documents/procedure.docx")
     question = state["question"]
-    prompt = f"""Contexte :{contenu}
-    Question :{question}
+    prompt = f"""
+        Historique :{historique}
+        Contexte :{contenu}
+        Question :{question}
     Réponse :
     """
     state["reponse"] = llm_local(prompt)
     return state
 
+
 def documentation_node(state):
+    historique = state["historique"]
     question = state["question"]
-    prompt = f"""Réponds à cette question :{question}"""
-    reponse = llm_local(prompt)
-    state["reponse"] = reponse
+
+    prompt = f"""
+    Historique :{historique}
+
+    Question :{question}
+
+    Réponse :
+    """
+    state["reponse"] = llm_local(prompt)
     return state
 
 
@@ -129,16 +168,17 @@ def greeting_node(state):
 def route_question(state):
     return state["type_question"]
 
+
 workflow = StateGraph(AgentState)
 
 workflow.add_node("analyse", analyse_node)
 workflow.add_node("decision", decision_node)
 workflow.add_node("calculatrice", calculatrice_node)
 workflow.add_node("txt_reader", txt_reader_node)
+workflow.add_node("pdf_reader", pdf_reader_node)
+workflow.add_node("docx_reader", docx_reader_node)
 workflow.add_node("documentation", documentation_node)
 workflow.add_node("salutation", greeting_node)
-workflow.add_node("pdf_reader",pdf_reader_node)
-workflow.add_node("docx_reader",docx_reader_node)
 
 workflow.set_entry_point("analyse")
 
@@ -150,8 +190,8 @@ workflow.add_conditional_edges(
     {
         "salutation": "salutation",
         "calcul": "calculatrice",
-        "pdf":"pdf_reader",
-        "docx":"docx_reader",
+        "pdf": "pdf_reader",
+        "docx": "docx_reader",
         "txt": "txt_reader",
         "documentation": "documentation",
     },
@@ -159,31 +199,51 @@ workflow.add_conditional_edges(
 
 workflow.add_edge("calculatrice", END)
 workflow.add_edge("txt_reader", END)
-workflow.add_edge("documentation", END)
-workflow.add_edge("salutation", END)
 workflow.add_edge("pdf_reader", END)
 workflow.add_edge("docx_reader", END)
-
-
+workflow.add_edge("documentation", END)
+workflow.add_edge("salutation", END)
 
 agent = workflow.compile()
 
+memoire = []
+
 questions = [
+    "Quels sont les congés ?",
     "Lis formation.pdf",
-    "Quels sujets sont étudiés ?",
-    "Lis procedure.docx",
-    "Que dit la procédure RH ?"
+    "50+20",
+    "Lis procedure.docx"
 ]
 
-for q in questions:
-    resultat = agent.invoke({"question": q})
-    print("\nQuestion :", q)
+for question in questions:
+
+    if question == "":
+        print("Veuillez saisir une question.")
+        continue
+
+    historique = "\n".join(memoire)
+
+    debut = time.time()
+
+    resultat = agent.invoke({
+        "question": question,
+        "historique": historique
+    })
+
+    fin = time.time()
+
+    reponse = resultat["reponse"]
+
+    memoire.append(f"Utilisateur : {question}")
+    memoire.append(f"Assistant : {reponse}")
+
+    print(reponse)
+    print("[LOG] Réponse générée")
+    print("Temps :", fin - debut, "secondes")
+    print("-------------")
+
+    print("\nQuestion :", question)
     print("Réponse :", resultat["reponse"])
 
-#print(pdf_reader("documents/formation.pdf"))
-#print(docx_reader("documents/procedure.docx"))
-
-#resultat = agent.invoke({"question":"What is Agent IA ?"})
-#print(resultat["reponse"])
-
-#print(llm_local("Bonjour"))
+    memoire.append(f"Utilisateur : {question}")
+    memoire.append(f"Assistant : {resultat['reponse']}")
